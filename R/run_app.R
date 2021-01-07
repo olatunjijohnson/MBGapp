a8194164599dc64c3c35a6ef886c28c3101c5171
olatunjijohnson <teejaystatistics@gmail.com>
2021-01-07 20:27:29 +0000

    first commit

--- R/run_app.R
+++ R/run_app.R
@@ -1,4 +1,9 @@
-#' Runs the SpatialEpiApp Shiny web application.
+# run_app.R
+#
+#' @title A function to run the app
+#' @description Run the app
+#' @details The function outputs the app for the user to supply the data and analysis the data
+#' Runs the MBGapp Shiny web application.
 #' @export
 run_app <- function() {
   appDir <- system.file('MBGapp', package='MBGapp')
