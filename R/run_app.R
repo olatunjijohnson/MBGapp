1ceac593178943d17a15168f5265f9d4d37119f8
olatunjijohnson <teejaystatistics@gmail.com>
2021-01-15 02:11:33 +0000

    add loaloa data to the package

--- R/run_app.R
+++ R/run_app.R
@@ -1,9 +1,8 @@
 # run_app.R
 #
 #' @title A function to run the app
-#' @description Run the app
+#' @description Runs the MBGapp Shiny web application.
 #' @details The function outputs the app for the user to supply the data and analysis the data
-#' Runs the MBGapp Shiny web application.
 #' @export
 run_app <- function() {
   appDir <- system.file('MBGapp', package='MBGapp')
